8f143565b3b1b9ea8b9b7d3a1f9c8d1d0ac9f9eb
mumal <113783501+mumal885@users.noreply.github.com>
2023-09-20 18:43:34 +0530

    Add files via upload

---
+++ src/components/Notification.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+
+const Notification=()=> {
+ return (
+
+   <div className="h-12 bg-red-500 text-white px-4 flex items-center justify-center text-center text-sm md:text-base cursor-pointer'">
+
+     <h1>Free delivery for all orders over $50. Order your food now! </h1>
+
+     
+
+   </div>
+
+ );
+
+}
+export default Notification
